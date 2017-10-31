2a9a22499b4b9b5549e2ea0373f01e29448ff46a
Richard Horton (hortonr6) <hortonr6@coventry.ac.uk>
2017-10-31 17:23:11 +0000

    Created demo GUI
    Currently nonfunctional but shows a simple example of a start screen. Will hopefully look better and become more polished soon.

---
+++ GUI.py
@@ -0,0 +1,17 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("Chatbot Jeff")
+window.geometry("600x800")
+window.configure(background="cornflower blue")
+
+lblTitle = tkinter.Label(window, text="CHATBOT JEFF\n\n", bg="cornflower blue", font=(24))
+lblTitle.pack()
+
+lblDesc = tkinter.Label(window, text="Welcome to Chatbot-Jeff\n\nThis chatbot was created by ALL group A1\n\n\n", bg="cornflower blue")
+lblDesc.pack()
+
+btnBegin = tkinter.Button(window, text="Click To Launch Chatbot")
+btnBegin.pack()
+
+window.mainloop()
